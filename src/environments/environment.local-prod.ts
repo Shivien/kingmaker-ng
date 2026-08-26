6b3779da36b011531daf2f217b93fb7970c9582b
Damien Leroy <d.leroy@matawan-services.com>
2026-08-26 20:54:10 +0200

    env local-prod et deploy pages

--- src/environments/environment.local-prod.ts
+++ src/environments/environment.local-prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://kingmaker-api-8yts.onrender.com',
+  production: false,
+  apiUrl: 'https://kingmaker-api-8yts.onrender.com/api',
   googleClientId: '169320809644-ivmc4cmcsooev48efakqclg8aqdumphb.apps.googleusercontent.com'
 };
