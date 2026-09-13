6025e617d7fbf27214aa8c7d82a9a3ddfe51e9a6
Damien Leroy <d.leroy@matawan-services.com>
2026-09-14 00:56:05 +0200

    OSE Temps morts

---
+++ src/app/ose/components/downtime/downtime.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-downtime',
+  styleUrl: './downtime.scss',
+  templateUrl: './downtime.html',
+})
+export class Downtime {}
---
+++ src/app/ose/components/dungeon-generator/dungeon-generator.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-dungeon-generator',
+  styleUrl: './dungeon-generator.scss',
+  templateUrl: './dungeon-generator.html',
+})
+export class DungeonGenerator {}
